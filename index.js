98a6f7aa51869250e50189e6960d3329f150d5d6
Volk Artem <rainbowsnake45@gmail.com>
2019-02-08 22:08:22 +0200

    index.js'

--- index.js
+++ index.js
@@ -20,3 +20,26 @@ process.on('unhandledRejection', (err) => {
 });
 
 init();
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: (request, h) => {
+
+        return 'Hello, ' + encodeURIComponent(request.params.name) + '!';
+    }
+});
+
+const init = async () => {
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
